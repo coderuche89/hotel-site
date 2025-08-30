b8c24388f79742653998c935eea198ce95058b39
unknown <you@example.com>
2025-08-31 00:30:26 +0100

    first commit

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_1lhd4lb";
+    const templateID = "template_7rb0zhb";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Thank you, we appreciate your feedback")
+        })
+        .catch(err => console.log(err));
+    
+}
